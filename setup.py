fb25ddfbba67196a2e576713720fc1142614cb50
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2022-01-26 09:38:41 +0100

    set dm_control revision to last revision supporting mujoco 2.1.0
    for mujoco > 2.1.0 paths appear to have changed.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                       'termcolor',  # adept_envs dependency
                       'click',  # adept_envs dependency
                       'dm_control' if 'macOS' in platform() else
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
+                      'dm_control @ git+https://github.com/deepmind/dm_control@644d9e0047f68b35a6f8b79e5e8493e2910563af',  # last version to support mujoco 2.1.0
                       'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
