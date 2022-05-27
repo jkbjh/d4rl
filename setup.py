d22d4391c07fc1fac9fb4b4621e4d04f483bd492
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2022-05-27 11:53:32 +0200

    change dependency to github 'https://' instead of 'git://' for mjrl.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
                       'click',  # adept_envs dependency
                       'dm_control' if 'macOS' in platform() else
                       'dm_control @ git+https://github.com/deepmind/dm_control@644d9e0047f68b35a6f8b79e5e8493e2910563af',  # last version to support mujoco 2.1.0
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl'],
     packages=find_packages(),
     package_data={'d4rl': ['locomotion/assets/*',
                            'hand_manipulation_suite/assets/*',
